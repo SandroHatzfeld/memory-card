7ff1996925c55f4a028f77c6b76618d645db6d09
Sandro Hatzfeld <sandrohatzfeld@gmail.com>
2025-04-28 13:24:34 +0200

    data merge

--- src/data/cardData.tsx
+++ src/data/cardData.tsx
@@ -1,87 +1,129 @@
-import angular from '../images/angular.jpg'
-import bootstrap from '../images/bootstrap.jpg'
-import css from '../images/css.jpg'
-import gsap from '../images/gsap.jpg'
-import html from '../images/html.jpg'
-import jquery from '../images/jquery.jpg'
-import js from '../images/js.jpg'
-import mysql from '../images/mysql.jpg'
-import node from '../images/node.jpg'
-import php from '../images/php.jpg'
-import react from '../images/react.jpg'
-import sass from '../images/sass.jpg'
-import vite from '../images/vite.jpg'
-import webpack from '../images/webpack.jpg'
+import angular from "./images/angular.jpg"
+import bootstrap from "./images/bootstrap.jpg"
+import css from "./images/css.jpg"
+import gsap from "./images/gsap.jpg"
+import html from "./images/html.jpg"
+import jquery from "./images/jquery.jpg"
+import js from "./images/js.jpg"
+import mysql from "./images/mysql.jpg"
+import node from "./images/node.jpg"
+import php from "./images/php.jpg"
+import react from "./images/react.jpg"
+import sass from "./images/sass.jpg"
+import vite from "./images/vite.jpg"
+import webpack from "./images/webpack.jpg"
+
+import vue from "./images/vue.jpg"
+import svelte from "./images/svelte.jpg"
+import typescript from "./images/typescript.jpg"
+import tailwind from "./images/tailwind.jpg"
+import npm from "./images/npm.jpg"
+import git from "./images/git.jpg"
+import python from "./images/python.jpg"
+import postgresql from "./images/postgresql.jpg"
+import mongodb from "./images/mongodb.jpg"
+import docker from "./images/docker.jpg"
+import graphql from "./images/graphql.jpg"
 
 export const cardData = [
-	{
-		name: "Angular",
-		image: angular,
-		clicked: false,		
-	},
-	{
-		name: "Bootstrap",
-		image: bootstrap,
-		clicked: false,		
-	},
-	{
-		name: "CSS",
-		image: css,
-		clicked: false,		
-	},
-	{
-		name: "GSAP",
-		image: gsap,
-		clicked: false,		
-	},
-	{
-		name: "HTML",
-		image: html,
-		clicked: false,		
-	},
-	{
-		name: "jQuery",
-		image: jquery,
-		clicked: false,		
-	},
-	{
-		name: "Javascript",
-		image: js,
-		clicked: false,		
-	},
-	{
-		name: "MySQL",
-		image: mysql,
-		clicked: false,		
-	},
-	{
-		name: "Node.js",
-		image: node,
-		clicked: false,		
-	},
-	{
-		name: "php",
-		image: php,
-		clicked: false,		
-	},
-	{
-		name: "React",
-		image: react,
-		clicked: false,		
-	},
-	{
-		name: "Sass",
-		image: sass,
-		clicked: false,		
-	},
-	{
-		name: "Vite",
-		image: vite,
-		clicked: false,		
-	},
-	{
-		name: "webpack",
-		image: webpack,
-		clicked: false,		
-	},
+  {
+    name: "Angular",
+    image: angular,
+  },
+  {
+    name: "Bootstrap",
+    image: bootstrap,
+  },
+  {
+    name: "CSS",
+    image: css,
+  },
+  {
+    name: "GSAP",
+    image: gsap,
+  },
+  {
+    name: "HTML",
+    image: html,
+  },
+  {
+    name: "jQuery",
+    image: jquery,
+  },
+  {
+    name: "Javascript",
+    image: js,
+  },
+  {
+    name: "MySQL",
+    image: mysql,
+  },
+  {
+    name: "Node.js",
+    image: node,
+  },
+  {
+    name: "php",
+    image: php,
+  },
+  {
+    name: "React",
+    image: react,
+  },
+  {
+    name: "Sass",
+    image: sass,
+  },
+  {
+    name: "Vite",
+    image: vite,
+  },
+  {
+    name: "webpack",
+    image: webpack,
+  },
+  {
+    name: "Vue.js",
+    image: vue,
+  },
+  {
+    name: "Svelte",
+    image: svelte,
+  },
+  {
+    name: "TypeScript",
+    image: typescript,
+  },
+  {
+    name: "Tailwind CSS",
+    image: tailwind,
+  },
+  {
+    name: "npm",
+    image: npm,
+  },
+  {
+    name: "Git",
+    image: git,
+  },
+  {
+    name: "Python",
+    image: python,
+  },
+  {
+    name: "PostgreSQL",
+    image: postgresql,
+  },
+  {
+    name: "MongoDB",
+    image: mongodb,
+  },
+  {
+    name: "Docker",
+    image: docker,
+  },
+  {
+    name: "GraphQL",
+    image: graphql,
+  },
 ]
